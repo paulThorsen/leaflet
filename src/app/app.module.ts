f849da33d5ddd73dd2a53500a06c1a12dc157cd8
Paul Thorsen <paulthorsen@Pauls-MacBook-Pro.local>
2020-08-31 09:10:20 -0600

    add item card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppComponent } from './app.component';
 import { ItemInfoCardComponent } from './item-info-card/item-info-card.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ItemTypeTextPipe } from './pipes/item-type-text-pipe'
-import { ItemTypeIconPipe } from './pipes/item-type-icon-pipe'
+import { ItemTypeIconPipe } from './pipes/item-type-icon-pipe';
+import { PrimaryButtonComponent } from './primary-button/primary-button.component'
 
 @NgModule({
-  declarations: [AppComponent, ItemInfoCardComponent, ItemTypeTextPipe, ItemTypeIconPipe],
+  declarations: [AppComponent, ItemInfoCardComponent, ItemTypeTextPipe, ItemTypeIconPipe, PrimaryButtonComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
